fe960fb252b192e33c7624caf63c63db8c19a2f3
Rabbits Foot <evansmaraah@gmail.com>
2023-04-20 10:44:06 +0300

    2nd commit

--- script.js
+++ script.js
@@ -26,30 +26,44 @@ function formValidation(){
 };
 
 
-let data ={}
-function acceptData(){
-  data["text"] = textInput.value;
-  data["date"] = dateInput.value;
-  data["description"] = taskArea.value;
-  console.log(data);
-  createTasks()
-}
-function createTasks(){
-  taskEvents.innerHTML +=`
-  <div>
-    <span>${data.text}</span><br><br>
-      <span>${data.date}</span><br><br>
-        <p>${data.description}</p><br>
-          <span class="options">
-              <ion-icon name="create-outline" onClick="updateTask(this)"></ion-icon>
-              <ion-icon name="trash-outline" onClick="deleteTask(this)"></ion-icon>
-          </span>
+let data = [{}];
 
-  </div>`          
+
+let acceptData=()=>{
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: taskArea.value,
+  });
+  localStorage.setItem("data",JSON.stringify(data));
+  console.log(data);
+  createTasks();
+}
+
+function createTasks(){
+  taskEvents.innerHTML= "";
+  data.map((x,y)=>{
+    return (taskEvents.innerHTML +=`
+    <div id=${y}>
+      <span>${x.text}</span><br><br>
+        <span>${x.date}</span><br><br>
+          <p>${x.description}</p><br>
+            <span class="options">
+                <ion-icon name="create-outline" id= "taskForm" onClick="updateTask(this)"></ion-icon>
+                <ion-icon name="trash-outline" createTasks() onClick="deleteTask(this)"></ion-icon>
+            </span>
+  
+    </div>`
+    );          
+  })
+  
   resetForm();
 }
 let deleteTask=(e)=>{
 e.parentElement.parentElement.remove();
+data.splice(e.parentElement.parentElement.id, 1);
+localStorage.setItem("data",JSON.stringify(data));
+console.log(data);
 }
 
 let updateTask=(e)=>{
@@ -57,6 +71,8 @@ let updateTask=(e)=>{
   textInput.value = selectedTask.children[0].innerHTML;
   dateInput.value = selectedTask.children[1].innerHTML;
   taskArea.value = selectedTask.children[2].innerHTML;
+  
+  deleteTask(e);
   };
 
 
@@ -65,3 +81,8 @@ function resetForm(){
   dateInput.value="";
   taskArea.value="";
 };
+
+(()=>{
+  data = JSON.parse(localStorage.getItem("data")) || [];
+  createTasks();
+})()
